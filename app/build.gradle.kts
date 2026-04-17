7e8c00ba3f389545530bddd949d1634fceee41f5
Olivia <olivia.meisohle@gmx.de>
2026-04-17 16:26:41 +0800

    Database initialized

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("androidx.room:room-runtime:2.8.4")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    annotationProcessor("androidx.room:room-compiler:2.8.4")
 }
