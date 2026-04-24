8fff4d135f85d8e166cd7084ef61c060225aa5a1
Olivia <olivia.meisohle@gmx.de>
2026-04-24 16:21:06 +0800

    Moved Expense and Overview Classes

--- app/src/main/java/com/github/budgetbuddy/Expense.java
+++ app/src/main/java/com/github/budgetbuddy/Expense.java
@@ -1,4 +1,4 @@
-package com.github.budgetbuddy.models;
+package com.github.budgetbuddy;
 
 import android.os.Bundle;
 
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import com.github.budgetbuddy.R;
 
 public class Expense extends AppCompatActivity {
 
